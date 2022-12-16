dbfcee42f831af5306d7b101fd4fcc3843b6423d
Angelo Salles <angelo.salles@mercadolivre.com>
2022-12-15 23:48:44 -0300

    finished

--- src/main/java/com/project/pubsub/SubscriberAsyncExample.java
+++ src/main/java/com/project/pubsub/SubscriberAsyncExample.java
@@ -1,17 +1,24 @@
 package com.project.pubsub;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
-
+import com.project.pubsub.model.schema.TestSchema;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import lombok.AllArgsConstructor;
 
-public class SubscribeAsyncExample {
+@AllArgsConstructor
+public class SubscriberAsyncExample {
 
-    public static void main(String... args) throws Exception {
+    private final ObjectMapper objectMapper;
+
+    public static void main(String... args) {
         // TODO(developer): Replace these variables before running the sample.
         String projectId = "charming-sonar-359523";
         String subscriptionId = "topic-a-sub";
@@ -29,6 +36,19 @@ public class SubscribeAsyncExample {
                     // Handle incoming message, then ack the received message.
                     System.out.println("Id: " + message.getMessageId());
                     System.out.println("Data: " + message.getData().toStringUtf8());
+
+                    TestSchema response = new TestSchema();
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        response = mapper.readValue(new String(message.getData().toByteArray(), StandardCharsets.UTF_8), TestSchema.class);
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (response.getMessage().equals("throw error")) {
+                        consumer.nack();
+                        return;
+                    }
                     consumer.ack();
                 };
 
--- src/main/java/com/project/pubsub/model/schema/TestSchema.java
+++ src/main/java/com/project/pubsub/model/schema/TestSchema.java
@@ -2,16 +2,17 @@ package com.project.pubsub.model.schema;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -26,6 +27,7 @@ public class TestSchema {
     private String message;
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime referredDate;
 }
